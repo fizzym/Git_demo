c230716615b4cd47404125c306d463194fb56ced
miti <you@example.com>
2020-06-09 13:29:14 -0700

    Added LED blinking feature.

--- src/main.cpp
+++ src/main.cpp
@@ -19,8 +19,14 @@ void setup() {
   display.println("Hello ENPH253");
   display.display();
   delay(1000);
+
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 //------------
 void loop() {
+  pinMode(LED_BUILTIN, HIGH);
+  delay(1000);
+  pinMode(LED_BUILTIN, LOW);
+  delay(1000);
 }

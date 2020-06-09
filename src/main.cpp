77be2e4e95bd509bd15f64ec0655a0256c3ea94b
miti <you@example.com>
2020-06-09 13:23:39 -0700

    Initial commit.

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
+//------------
+
+#define OLED_RESET -1  // Not used
+Adafruit_SSD1306 display(OLED_RESET);
+
+//------------
+void setup() {
+  // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x64)
+  // init done
+
+  // Draw a test
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.println("Hello ENPH253");
+  display.display();
+  delay(1000);
+}
+
+//------------
+void loop() {
+}
